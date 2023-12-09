1c123bdfa532d732f50bf124ec7599df5bf56f26
va.efremov <vlad.efrrvov@mail.ru>
2023-12-09 11:55:07 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ksc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ksc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const imStyleText = TextStyle(
+  fontSize: 20,
+  color: Color.fromARGB(100, 0, 255, 235),
+  fontWeight: FontWeight.bold
+);
