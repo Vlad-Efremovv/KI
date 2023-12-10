83adb29dd231da4d16a58895022d57b132289b8f
va.efremov <vlad.efrrvov@mail.ru>
2023-12-10 14:31:41 +0300

    сделал asset и пытаюсь сделать титульник

--- lib/style.dart
+++ lib/style.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+//const imBackgroundColor = Color.fromARGB(70, 0, 240, 230);
+
 const imStyleText = TextStyle(
-  fontSize: 20,
-  color: Color.fromARGB(100, 0, 255, 235),
-  fontWeight: FontWeight.bold
-);
+    fontSize: 30,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Andale Mono');
